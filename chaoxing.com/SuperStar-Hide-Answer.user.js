7937ced494912982b58fb9d7040e08955ed08c84
甜檸seril <45784494+lcandy2@users.noreply.github.com>
2023-06-30 12:03:32 +0800

    SuperStar-Hide-Answer: Add support for exams

--- chaoxing.com/SuperStar-Hide-Answer.user.js
+++ chaoxing.com/SuperStar-Hide-Answer.user.js
@@ -1,18 +1,18 @@
 // ==UserScript==
 // @name         学习通隐藏答案
 // @namespace    https://github.com/lcandy2
-// @version      1.2
+// @version      1.3
 // @description  添加一个切换答案按钮，点击可显示/隐藏答案
 // @author       Lcandy
 // @match        *://mooc1.chaoxing.com/mooc*
+// @match        *://mooc1.chaoxing.com/exam*
 // @homepage     https://github.com/lcandy2/user.js
 // @run-at       document-end
 // @grant        none
 // ==/UserScript==
 
 (function() {
-  // 检查 URL 是否包含 "work/view"
-  if (window.location.href.includes("work/view")) {
+  if (window.location.href.includes("work/view") || window.location.href.includes("test/reVersionPaperMarkContentNew")) {
     const targetElement = document.querySelector('.subNav');
     const toggleButton = document.createElement('div');
     toggleButton.className = 'sub-button fr';

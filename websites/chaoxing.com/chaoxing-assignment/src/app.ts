8400bab5e5566631dd5a5b6b856b40198e03d6ec
甜檸Cirtron <45784494+lcandy2@users.noreply.github.com>
2024-05-18 01:22:06 +0800

    refactor(chaoxing-assignment): init with svelte

--- websites/chaoxing.com/chaoxing-assignment/src/app.ts
+++ websites/chaoxing.com/chaoxing-assignment/src/app.ts
@@ -1,31 +1,11 @@
-// Vuetify
-import 'vuetify/dist/vuetify.css'
-import { createVuetify } from 'vuetify'
-import { createApp } from "vue";
-import App from "./App.vue";
-import './lib/material-icon-fonts.css'
-import { aliases, md } from "vuetify/iconsets/md";
-// import * as components from 'vuetify/components'
-// import * as directives from 'vuetify/directives'
+import App from "./App.svelte";
 
 export const appendApp = () => {
-  const vuetify = createVuetify({
-    // components,
-    // directives,
-    icons: {
-      defaultSet: 'md',
-      aliases,
-      sets: {
-        md,
-      },
-    },
-  })
-  // const vuetify = createVuetify()
-  createApp(App).use(vuetify).mount(
-    (() => {
-      const app = document.createElement("div");
+  return new App({
+    target: (() => {
+      const app = document.createElement('div');
       document.body.append(app);
       return app;
     })(),
-  );
+  });
 }
--- websites/chaoxing.com/chaoxing-assignment/vite.config.ts
+++ websites/chaoxing.com/chaoxing-assignment/vite.config.ts
@@ -1,15 +1,13 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import monkey, { cdn } from "vite-plugin-monkey";
-import vuetify from 'vite-plugin-vuetify'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import monkey from 'vite-plugin-monkey';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    vuetify(),
+    svelte(),
     monkey({
-      entry: "src/main.ts",
+      entry: 'src/main.ts',
       userscript: {
         name: "【学习通任务一览】- 作业 与 考试 列表 | 电脑端快速查看，绝不错过任何作业与考试",
         namespace: "https://github.com/lcandy2/user.js/tree/main/websites/chaoxing.com/chaoxing-assignment",
@@ -19,18 +17,6 @@ export default defineConfig({
           "*://i.chaoxing.com/base*",
         ],
         "run-at": "document-end",
-      },
-      build: {
-        externalGlobals: {
-          vue: cdn.npmmirror("Vue", "dist/vue.global.prod.js"),
-          // vuetify: cdn.npmmirror("Vuetify", "dist/vuetify.min.js"),
-          // "vuetify/components": "Vuetify",
-          // "vuetify/directives": "Vuetify",
-          // "vuetify/iconsets/md": "Vuetify",
-        },
-        externalResource: {
-          'vuetify/dist/vuetify.css': cdn.npmmirror("vuetify/dist/vuetify.css", "dist/vuetify.min.css"),
-        }
       },
     }),
   ],

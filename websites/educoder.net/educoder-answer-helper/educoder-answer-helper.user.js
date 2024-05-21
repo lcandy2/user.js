d8eaa15a017d9c42ab9c2d89153140ff030d5ccb
甜檸Cirtron <45784494+lcandy2@users.noreply.github.com>
2024-05-21 21:10:25 +0800

    feat(educoder-helper): release 1.5(answer)

--- websites/educoder.net/educoder-answer-helper/educoder-answer-helper.user.js
+++ websites/educoder.net/educoder-answer-helper/educoder-answer-helper.user.js
@@ -1,17 +1,18 @@
 // ==UserScript==
-// @name       educoder-answer-helper
-// @namespace  educoder-answer-helper
-// @version    0.1
-// @author     monkey
-// @license    MIT
-// @match      *://www.educoder.net/tasks/*
-// @grant      none
-// @run-at     document-start
+// @name         educoder-answer-helper
+// @namespace    educoder-answer-helper
+// @version      0.2
+// @author       monkey
+// @description  educoder-answer-helper
+// @license      MIT
+// @match        *://www.educoder.net/tasks/*
+// @grant        none
+// @run-at       document-start
 // ==/UserScript==
 
 (function () {
 	'use strict';
 
-	window.educoderAnswerHelper = "0.1";
+	window.educoderAnswerHelper = "0.2";
 
 })();

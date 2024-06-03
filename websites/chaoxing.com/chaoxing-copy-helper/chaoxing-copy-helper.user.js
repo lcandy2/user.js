048dfb6217746468bba12b7335b352619190fd49
甜檸Cirtron <45784494+lcandy2@users.noreply.github.com>
2024-06-03 21:49:12 +0800

    feat(chaoxing-copy-helper): release 1.1

--- websites/chaoxing.com/chaoxing-copy-helper/chaoxing-copy-helper.user.js
+++ websites/chaoxing.com/chaoxing-copy-helper/chaoxing-copy-helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         学习通复制粘贴助手 Chaoxing Copy Helper
 // @namespace    https://github.com/lcandy2/user.js/tree/main/websites/chaoxing.com/chaoxing-copy-helper
-// @version      1.0
+// @version      1.1
 // @author       甜檸Cirtron (lcandy2)
 // @description  📝解除超星学习通复制粘贴限制，解除学习通复制乱码问题；✨“一键复制题目”、“一键粘贴到答案”、“复制好友答案”等功能。🧹大小仅1.62KB，极小尺寸，无需任何权限，无需任何配置，安装即用。💛安全开源可读，无附加功能或远程代码，防止窃取其他信息
 // @license      AGPL-3.0-or-later
@@ -33,7 +33,7 @@
       const markInfo = document.querySelector(".mark_info");
       if (markInfo) {
         const branding = document.createElement("p");
-        branding.innerHTML = `已解除复制/粘贴限制<br><span style="font-size: 12px;">- 由 学习通复制粘贴助手 Chaoxing Copy Helper 提供 -</span>`;
+        branding.innerHTML = `已解除复制/粘贴限制<br><span style="font-size: 12px;">- 由 <a href="https://greasyfork.org/scripts/496958" target="_blank">学习通复制粘贴助手 Chaoxing Copy Helper 提供</a> -</span>`;
         markInfo.appendChild(branding);
       }
     }
--- websites/chaoxing.com/chaoxing-copy-helper/src/lib/branding.ts
+++ websites/chaoxing.com/chaoxing-copy-helper/src/lib/branding.ts
@@ -9,7 +9,7 @@ const addExamBranding = () => {
     const markInfo = document.querySelector(".mark_info");
     if (markInfo) {
       const branding = document.createElement("p");
-      branding.innerHTML = `已解除复制/粘贴限制<br><span style="font-size: 12px;">- 由 学习通复制粘贴助手 Chaoxing Copy Helper 提供 -</span>`;
+      branding.innerHTML = `已解除复制/粘贴限制<br><span style="font-size: 12px;">- 由 <a href="https://greasyfork.org/scripts/496958" target="_blank">学习通复制粘贴助手 Chaoxing Copy Helper 提供</a> -</span>`;
       markInfo.appendChild(branding);
     }
   }

7bbd52dc143eda24c3dfb987e5bec28ef3963b67
甜檸Cirtron <45784494+lcandy2@users.noreply.github.com>
2024-06-03 20:25:06 +0800

    feat(hide-answer): agpl license

--- generics/hide-answer/hide-answer.user.js
+++ generics/hide-answer/hide-answer.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         学习通/MOOC等 隐藏答案 Hide Answer
 // @namespace    https://github.com/lcandy2/user.js/tree/main/generics/hide-answer
-// @version      2.1
+// @version      2.1.1
 // @author       monkey
 // @description  添加一个切换答案按钮，点击可显示/隐藏答案
+// @license      AGPL-3.0-or-later
 // @match        *://mooc1.chaoxing.com/mooc*
 // @match        *://mooc1.chaoxing.com/exam*
 // @require      https://registry.npmmirror.com/vue/3.4.27/files/dist/vue.global.prod.js
@@ -571,7 +572,9 @@
     __name: "chaoxing-mooc",
     setup(__props) {
       const snackbar = Ee();
-      const elements = Array.from(document.getElementsByClassName("mark_answer"));
+      const elements = Array.from(
+        document.getElementsByClassName("mark_answer")
+      );
       const isHide = vue.ref(false);
       const toggleHide = () => {
         const value = isHide.value;
